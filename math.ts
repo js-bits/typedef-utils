0562c95f5e22c978fa5e64883c951cb20d33fe36
ApoloGH <apolo-gh@users.noreply.github.com>
2023-05-26 15:24:53 -0400

    refactor: add ParseInt type

--- math.ts
+++ math.ts
@@ -1,15 +1,15 @@
 // INSPIRATION: https://itnext.io/implementing-arithmetic-within-typescripts-type-system-a1ef140a6f6f
 type Length<T extends any[]> = T extends { length: infer L } ? L : never;
 
-type BuildTuple<L extends number, T extends any[] = []> = T extends {
-  length: L;
-}
-  ? T
-  : BuildTuple<L, [...T, any]>;
+type BuildTuple<L extends number, T extends any[] = []> = T extends { length: L } ? T : BuildTuple<L, [...T, any]>;
 
 export type Increment<Start extends number = 0, Inc extends number = 1> = Length<
   [...BuildTuple<Inc>, ...BuildTuple<Start>]
 >;
+
+// https://github.com/microsoft/TypeScript/pull/48094
+// requires TypeScript 4.8+
+export type ParseInt<T> = T extends `${infer N extends number}` ? N : never;
 
 type x = Increment<5>;
 
