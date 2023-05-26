ee2ab96b92237d4e3cf4a092448ac0f319162eab
ApoloGH <apolo-gh@users.noreply.github.com>
2023-05-26 18:47:27 -0400

    refactor: optimize math utils performance

--- src/math.ts
+++ src/math.ts
@@ -9,7 +9,9 @@
 // requires TypeScript 4.8+
 export type ParseInt<T> = T extends `${infer N extends number}` ? N : never;
 
-type Length<T extends any[]> = T extends { length: infer L } ? L : never;
+type Length<T extends unknown[]> = T extends { length: infer L } ? L : never;
+
+type Longest<A extends unknown[], B extends unknown[]> = B[Length<A>] extends undefined ? A : B;
 
 type Digits = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9';
 type Carryings = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
@@ -26,18 +28,28 @@ type DigitSums = [
   [8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
   [9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
 ];
-type SplitDigits<Num extends string, A extends Digits[] = []> = Num extends `${infer D extends Digits}${infer X}`
-  ? SplitDigits<X, [D, ...A]>
-  : A;
-type Pad<Num extends string> = `000000000000000000000000${Num}`;
+
+type SplitDigits<
+  Num extends string | number,
+  A extends Digits[] = []
+> = `${Num}` extends `${infer D extends Digits}${infer X}` ? SplitDigits<X, [D, ...A]> : [...A, '0'];
+
+type SumDigits<A extends Digits = '0', B extends Digits = '0'> = DigitSums[A extends undefined
+  ? '0'
+  : A][B extends undefined ? '0' : B];
+
 type AddString<
-  A extends string,
-  B extends string,
-  NumA extends Digits[] = SplitDigits<Pad<A>>,
-  NumB extends Digits[] = SplitDigits<Pad<B>>
+  A extends string | number,
+  B extends string | number,
+  NumA extends Digits[] = SplitDigits<A>,
+  NumB extends Digits[] = SplitDigits<B>,
+  L extends Digits[] = Longest<NumA, NumB>
 > = {
-  [Index in keyof NumA]: SumDigits<NumA[Index], NumB[Index]>;
+  [Index in keyof L]: SumDigits<NumA[ParseInt<Index>], NumB[ParseInt<Index>]>;
 };
+
+type ass = Longest<['3'], SplitDigits<'5'>>;
+type as = AddString<'3', '8'>;
 
 type ToNumber<A extends number[], Result extends string = '', Carrying extends number = 0> = A extends [
   infer D extends number,
@@ -48,35 +60,10 @@ type ToNumber<A extends number[], Result extends string = '', Carrying extends n
 
 type Normalize<Str extends string> = Str extends `0${infer Num}` ? Normalize<Num> : Str;
 
-type Increment<A extends number | string> = ParseInt<
-  ToNumber<AddString<`0${A}`, '1', SplitDigits<`${A}`>, ['1', '0', '0', '0', '0', '0', '0']>>
->;
+export type Add<A extends string | number, B extends string | number> = ParseInt<Normalize<ToNumber<AddString<A, B>>>>;
 
-export type Add<A extends string | number, B extends string | number> = ParseInt<
-  Normalize<ToNumber<AddString<`${A}`, `${B}`>>>
->;
-
-type III = Increment<1999>;
-const II: Add<1999, 1> = 10;
-
-// 4 X 5 = 5 + 5 + 5 + 5
-export type Multiply2<
-  A extends string | number,
-  B extends string | number,
-  Result extends string = Pad<'0'>,
-  I extends number = 0,
-  NumA extends string[] = SplitDigits<Pad<`${A}`>>,
-  NumB extends string[] = SplitDigits<Pad<`${B}`>>
-> = I extends A
-  ? ParseInt<Normalize<Result>>
-  : Multiply2<
-      A,
-      B,
-      ToNumber<AddString<Result, `${B}`, SplitDigits<Result>, NumB>>,
-      /* Add<I, 1> */ Increment<I>,
-      NumA,
-      NumB
-    >;
+// type III = Increment<1999>;
+const II: Add<1999, 1> = 2000;
 
 export type Multiply<
   A extends string | number,
@@ -87,28 +74,17 @@ export type Multiply<
   Inc extends number = Add<I, 1>
 > = I extends A ? Result : Multiply<A, B, X, Inc>;
 
-type YY = ParseInt<Normalize<ToNumber<AddString<'10', '1', SplitDigits<'19'>, ['1']>>>>;
-
 const tn1: Add<9, 1> = 10;
 const tn2: Add<99, 1> = 100;
 const tn3: Add<999, 1> = 1000;
 const tn4: Add<999, 999> = 1998;
-const add: AddString<'99', '1'>[0] = 10;
-const tnu: ToNumber<AddString<'99', '1'>> = '00000000000000000000000100';
+const add: AddString<'99', '1'> = [10, 9, 0];
+const tnu: ToNumber<AddString<'99', '1'>> = '100';
 const add1: Add<9, 99999999999> = 100000000008;
 const add2: Add<99999999999, 9> = 100000000008;
 
 // A is limited to 999, B is "limitless"
 const mu: Multiply<999, 99999999> = 99899999001;
 const mu2: Multiply<199, 3> = 597;
-
-// const xxx =199 * 3
-// const xx: mu =xxx
-
-type d = SplitDigits<Pad<'012'>>;
-// AA extends Digits = A extends unknown ? 0 : A, BB extends Digits = B extends unknown ? 0 : B
-type SumDigits<A extends Digits = '0', B extends Digits = '0'> = DigitSums[A extends undefined
-  ? '0'
-  : A][B extends undefined ? '0' : B];
 
 type ss = SumDigits<'9', '9'>;
